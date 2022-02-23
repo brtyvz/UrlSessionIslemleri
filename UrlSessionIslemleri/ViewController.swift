ac018290f24651e64952e3c1fdc709faec7bcf54
brtyvz <beratttyavuzz@gmail.com>
2022-02-23 11:50:36 +0300

    Initial Commit

---
+++ UrlSessionIslemleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrlSessionIslemleri
+//
+//  Created by Berat Yavuz on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
